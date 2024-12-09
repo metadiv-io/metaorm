9c28af82cf138661a84fceca0c0504fb94d8cc98
Nelson Lai <nelsonlai9503@gmail.com>
2024-12-09 11:33:34 +0800

    refactor encryption

--- encryption.go
+++ encryption.go
@@ -6,12 +6,22 @@ import (
 	"github.com/metadiv-io/aes"
 )
 
-// Encrypt encrypts the given text using the METAORM_ENCRYPT_KEY environment variable.
-func Encrypt(text string) []byte {
-	return aes.Encrypt.StringToBytes(text, os.Getenv("METAORM_ENCRYPT_KEY"))
+// Encrypt encrypts the given text using the given encrypt key.
+func Encrypt(text string, encryptKey string) []byte {
+	return aes.Encrypt.StringToBytes(text, encryptKey)
 }
 
-// Decrypt decrypts the given cipher text using the METAORM_ENCRYPT_KEY environment variable.
-func Decrypt(cipherText []byte) string {
-	return aes.Decrypt.BytesToString(cipherText, os.Getenv("METAORM_ENCRYPT_KEY"))
+// DefaultEncrypt encrypts the given text using the METAORM_ENCRYPT_KEY environment variable.
+func DefaultEncrypt(text string) []byte {
+	return Encrypt(text, os.Getenv("METAORM_ENCRYPT_KEY"))
+}
+
+// Decrypt decrypts the given cipher text using the given decrypt key.
+func Decrypt(cipherText []byte, decryptKey string) string {
+	return aes.Decrypt.BytesToString(cipherText, decryptKey)
+}
+
+// DefaultDecrypt decrypts the given cipher text using the METAORM_ENCRYPT_KEY environment variable.
+func DefaultDecrypt(cipherText []byte) string {
+	return Decrypt(cipherText, os.Getenv("METAORM_ENCRYPT_KEY"))
 }

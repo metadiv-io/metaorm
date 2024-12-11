e80ea16ecdc4591adc38f9f2c96ec590a0116e06
Nelson Lai <nelsonlai9503@gmail.com>
2024-12-12 02:30:23 +0800

    adjust safe field for *

--- query/util.go
+++ query/util.go
@@ -3,7 +3,12 @@ package query
 import "strings"
 
 func SafeField(field string) string {
+	var prefix string
+	if strings.HasPrefix(field, "*") {
+		prefix = "*"
+		field = strings.TrimPrefix(field, "*")
+	}
 	field = strings.ReplaceAll(field, "`", "")
 	field = strings.ReplaceAll(field, ".", "`.`")
-	return "`" + field + "`"
+	return prefix + "`" + field + "`"
 }
